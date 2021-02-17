1b889c7dcd9cff31592ba63e3d5ed688a79f0e0b
Fernando Ocaña <ferrubocana@gmail.com>
2021-02-16 22:19:14 -0600

    El botón de la aplicación ya responde ante un click

--- Project/app/src/main/java/com/froi/graficador/MainActivity.java
+++ Project/app/src/main/java/com/froi/graficador/MainActivity.java
@@ -1,14 +1,28 @@
 package com.froi.graficador;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtArea=(EditText)findViewById(R.id.txtArea);
     }
+
+    public void compilacion(View view) {
+
+        System.out.println(txtArea.getText().toString());
+
+    }
+
 }
